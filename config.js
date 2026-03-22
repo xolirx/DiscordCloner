268d9135a29b9ad48b6caad57a92ac59b0bbf341
xolirx <fendihan7765@gmail.com>
2026-03-22 20:11:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window.CONFIG = {
     maxLogs: 200,
     apiBase: 'https://discord.com/api/v10',
-    corsProxy: 'https://cors-anywhere.herokuapp.com/',
     retryAttempts: 3,
     rateLimitDelay: 1000,
-    maxConcurrent: 10,
+    maxConcurrent: 5,
     requestTimeout: 30000,
     cacheTTL: 300000,
-    maxGuilds: 100
+    maxGuilds: 100,
+    useProxy: false
 };
